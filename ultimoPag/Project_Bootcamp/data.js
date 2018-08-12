3076372aad8cfceec5395e3cc152d3a1c85cf490
Juan Berti <juanluisberti@gmail.com>
2018-08-12 20:48:09 -0300

    data.js

---
+++ ultimoPag/Project_Bootcamp/data.js
@@ -0,0 +1,56 @@
+class Property{
+    constructor (id, name, image, zoneId, propertyType, operationType, details, place, address, isHighligth)
+    {
+        this.id = id;
+        this.name = name;
+        this.imge = image;
+        this.zoneId = zoneId;
+        this.propertyType = propertyType;
+        this.operationType = operationType;
+        this.details = details;
+        this.place = place;
+        this.address = address;
+        this.isHighligth = isHighligth;
+    }
+}
+
+var zone = [
+	{
+		id: 1,
+		name: 'Montevideo'
+    },
+    {
+		id: 2,
+		name: 'Malvin'
+    },
+    {
+		id: 3,
+		name: 'Pocitos'
+    },
+    {
+		id: 4,
+		name: 'Carrasco'
+    },
+    {
+		id: 5,
+		name: 'Punta carretas'
+    }
+];
+
+const propertyType = {HOUSE:'Casa', APTO:'Apto', BUSINESS:'Negocio'};
+const operationType = {SALE:'venta',  RENT:'renta'};
+const detailsTypes = {SIZE: 'metraje'};
+
+function initialize(contenedor){
+  console.log(contenedor);
+  unInmueble = new Property(1, 'CasaLopez', 'img', 1 , propertyType.HOUSE, operationType.SALE, detailsTypes.SIZE, {lat:111, lng: 1111}, 'Paraguay 2141', true);
+  contenedor.push(unInmueble);
+
+}
+
+
+$( document ).ready(function() {
+		var propiedades = [];
+		initialize(propiedades);
+		console.log(propiedades);
+	  });
