20e6d772e1feb2619fb58e571d2957e03cff5ae1
Juan Berti <juanluisberti@gmail.com>
2018-08-13 16:52:39 -0300

    pag3Carrusel

---
+++ ultimoPag/Project_Bootcamp/Pagina3UltimoCarrusel/paginaTres.js
@@ -0,0 +1,20 @@
+
+
+
+
+$('#myCarouselCustom').carousel();
+
+// Go to the previous item
+$("#prevBtn").click(function(){
+    $("#myCarouselCustom").carousel("prev");
+});
+// Go to the previous item
+$("#nextBtn").click(function(){
+    $("#myCarouselCustom").carousel("next");
+});
+
+$('.carousel').carousel({
+    interval: 8000,
+    pause:true,
+    wrap:false
+});
